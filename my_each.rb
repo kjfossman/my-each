1babdef89a918273a06f21fb808852839a6db1fa
Kyle <kjfossman@gmail.com>
2021-02-03 13:39:19 -0900

    commit tree ee980e0b7f8887d61b48f7927b344abea3b81551

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array)
+  i = 0
+  new_array = []
+  while i < array.length do 
+    yield(array[i])
+    
+    i = i + 1
+  end
+ return array
+end
+
+collection = ["hi", "hello", "bye", "goodbye"]
+
+my_each(collection) do |num|
+   num
 end
